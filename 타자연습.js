d325feee5d6ddca5759a8f3eb138af61d603584b
smp2103 <60417984+smp2103@users.noreply.github.com>
2020-05-22 15:51:46 +0900

    Update 타자연습.js

--- 타자연습.js
+++ 타자연습.js
@@ -307,23 +307,23 @@ function showingTier(number) {
         yourTier.innerText=`${tierName[4]}`
         yourTierDescription.innerText=`${tierDescription[4]}` 
         meanSpeedValue.innerText= `${meanSpeed}`
-        paintImage("https://raw.githubusercontent.com/smp2103/howfast/master/%EC%99%95.PNG?token=AOM6PQDDCCDTXYKR7NF2YUK6Y5ZN2")   
+        paintImage("https://raw.githubusercontent.com/smp2103/howfast/master/%EC%99%95.PNG?token=AOM6PQDAEKG2HNAF2RSFJ7K6Y53GC")   
     } else if(number>180) {
         yourTier.innerText=`${tierName[3]}`
         yourTierDescription.innerText=`${tierDescription[3]}`
-        paintImage("https://raw.githubusercontent.com/smp2103/howfast/master/%EC%96%91%EB%B0%98.PNG?token=AOM6PQF3INXFC24S3YIXQUK6Y5ZO6")   
+        paintImage("https://raw.githubusercontent.com/smp2103/howfast/master/%EC%96%91%EB%B0%98.PNG?token=AOM6PQBGG2DC4MUSOCSCIYC6Y53HO")   
     } else if(number>130) {
         yourTier.innerText=`${tierName[2]}`
         yourTierDescription.innerText=`${tierDescription[2]}`
-        paintImage("https://raw.githubusercontent.com/smp2103/howfast/master/%ED%8F%89%EB%AF%BC.PNG?token=AOM6PQHLRDZVRZ2YHMABDCK6Y5ZQE")   
+        paintImage("https://raw.githubusercontent.com/smp2103/howfast/master/%ED%8F%89%EB%AF%BC.PNG?token=AOM6PQAW23CZ5PD25YPDNYK6Y53IO")   
     } else if (number>100) {
         yourTier.innerText=`${tierName[1]}`
         yourTierDescription.innerText=`${tierDescription[1]}`
-        paintImage("https://raw.githubusercontent.com/smp2103/howfast/master/%EB%85%B8%EC%98%88.PNG?token=AOM6PQDAAQW4O74FCAPXYHK6Y5ZRE")   
+        paintImage("https://raw.githubusercontent.com/smp2103/howfast/master/%EB%85%B8%EC%98%88.PNG?token=AOM6PQHLDHZZSXD5AC3XHGC6Y53JS")   
     } else {
         yourTier.innerText=`${tierName[0]}`
         yourTierDescription.innerText=`${tierDescription[0]}`
-        paintImage("https://raw.githubusercontent.com/smp2103/howfast/master/%EB%98%A5.PNG?token=AOM6PQB65ZCA4RZ2NSKNOTS6Y52XE")
+        paintImage("https://raw.githubusercontent.com/smp2103/howfast/master/%EB%98%A5.PNG?token=AOM6PQAZEEXRVX2TB3FR3P26Y53LG")
     }
 
 }
